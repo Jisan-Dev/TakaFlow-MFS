4e1ea2694f54582a3411cd8a685e1b540a8d87a7
Jisun696 <istiakkashem35@gmail.com>
2024-07-29 01:16:47 +0600

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 let jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.chn7ebi.mongodb.net/?appName=Cluster0`;
 
 app.use(
   cors({
@@ -11,6 +13,24 @@ app.use(
   })
 );
 app.use(express.json());
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('hello world from takaFlow server');
