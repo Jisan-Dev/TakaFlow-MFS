f5e85c59071b9a50f26272c36b67bd920f69dedf
Jisun696 <istiakkashem35@gmail.com>
2024-08-19 22:11:21 +0600

    updated ui

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -24,31 +24,24 @@ const Sidebar = () => {
   return (
     <>
       {/* Small Screen Navbar */}
-      <div className="bg-gray-100 text-gray-800 flex justify-between md:hidden">
+      <div className="bg-indigo-100/20 text-gray-800 flex justify-between md:hidden">
         <div>
           <div className="block cursor-pointer p-4 font-bold">
-            <h1>Hello</h1>
-            {/* <Link to="/">
-              <img
-                // className='hidden md:block'
-                src="https://i.ibb.co/4ZXzmq5/logo.png"
-                alt="logo"
-                width="100"
-                height="100"
-              />
-            </Link> */}
+            <Link to={'/'} className="text-2xl font-semibold font-ibmsans flex items-center gap-1 text-indigo-600">
+              <GiCash className="text-4xl" /> TakaFlow
+            </Link>
           </div>
         </div>
 
-        <button onClick={handleToggle} className="mobile-menu-button p-4 focus:outline-none focus:bg-gray-200">
-          <AiOutlineBars className="h-5 w-5" />
+        <button onClick={handleToggle} className="mobile-menu-button p-4 focus:outline-none ">
+          <AiOutlineBars className="h-5 w-5 text-indigo-800" />
         </button>
       </div>
 
       {/* Sidebar */}
       <div
         className={`z-10 md:fixed flex flex-col justify-between overflow-x-hidden bg-purple-100 w-64 space-y-6 px-2 py-4 absolute inset-y-0 left-0 transform ${
-          isActive && '-translate-x-full'
+          !isActive && '-translate-x-full'
         }  md:translate-x-0  transition duration-200 ease-in-out`}>
         <div>
           <div>
--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -2,24 +2,16 @@ import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import { Login } from '@/components/auth/Login';
 import { SignUp } from '@/components/auth/SignUp';
+import Home from '@/pages/home/Home';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Login />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/sign-up',
-    element: <SignUp />,
-  },
-  {
-    path: '/dashboard',
     element: <MainLayout />,
+    children: [{ path: '/', element: <Home /> }],
   },
+  { path: '/login', element: <Login /> },
+  { path: '/sign-up', element: <SignUp /> },
 ]);
 
 export default router;
