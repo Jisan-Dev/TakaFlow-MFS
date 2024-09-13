f345a279f87df206468d14c825308cf55c1b3845
Jisun696 <istiakkashem35@gmail.com>
2024-09-14 03:44:57 +0600

    UI update with actual data

--- client/src/pages/user/UserOverview.jsx
+++ client/src/pages/user/UserOverview.jsx
@@ -8,11 +8,13 @@ const UserOverview = () => {
     <>
       <div className="w-full flex justify-between items-center border-b border-b-slate-600 pb-4  mb-6">
         <div className="text-white">
-          <p className="text-xl font-medium">Istiak Jisan</p>
-          <p className="text-sm">Account Type: Personal</p>
+          <p className="text-xl font-medium capitalize"> {user?.name} </p>
+          <p className="text-sm">
+            Account Type: <span className="capitalize"> {user?.role} </span>{' '}
+          </p>
         </div>
         <div className="text-white bg-white/20 backdrop-blur-3xl p-4 rounded-md">
-          <p>Balance: 200</p>
+          <p>Balance: {user?.balance} BDT </p>
         </div>
       </div>
       <div className="grid grid-cols-3 gap-6">
